9e568a1f937e20726eb68d7027386fcc531dddae
Choaib ELMADI <choaibamd@gmail.com>
2025-01-01 19:42:57 +0100

    Minor program updates

--- Programs/Main/Main.ino
+++ Programs/Main/Main.ino
@@ -1,6 +1,9 @@
 #include <U8g2lib.h>
 #include <Wire.h>
 
+#define R1 6.58
+#define R2 9.57
+
 const float a = -0.022;
 const float b = 8.646;
 
@@ -10,15 +13,14 @@ const uint8_t buttonPin = A0;
 bool buttonPressed = false;
 uint8_t state = 0;
 
-int voltageInPin;
+int voltageInPin = 0;
 float inputVoltage, trueInputVoltage;
 float level, voltage;
 
-void drawGraph(int8_t);
-void drawPint(int, int, int8_t);
+void drawGraph(float, float, int8_t);
+void drawPint(float, float, int8_t);
 
 void setup() {
-    Serial.begin(9600);
     u8g2.begin();
 
     pinMode(buttonPin, INPUT);
@@ -37,14 +39,16 @@ void loop() {
 
     voltageInPin = analogRead(A1);
     inputVoltage = voltageInPin * (5.0 / 1023.0);
-    trueInputVoltage = (16.15 / 6.58) * inputVoltage;
+    trueInputVoltage = ((R1 + R2) / R1) * inputVoltage;
 
     if (trueInputVoltage > b) {
         trueInputVoltage = b;
     }
 
-    voltage = b - trueInputVoltage;
     level = (trueInputVoltage - b) / a;
+
+    voltage = b - trueInputVoltage;
+    voltage = map(voltage, 0, b, b, 0);
 
     u8g2.clearBuffer();
 
@@ -60,49 +64,50 @@ void loop() {
         u8g2.setCursor(0, 45);
         u8g2.print("Niveau  :  " + String(level));
         u8g2.setCursor(0, 60);
-        u8g2.print("Tension:  " + String(voltage) + " V");
+        u8g2.print("CMD      :  " + String(voltage) + " V");
         break;
     case 1:
-        drawGraph(1);
-        drawPoint((int)level, (int)trueInputVoltage, 1);
+        drawGraph(level, trueInputVoltage, 1);
+        drawPoint(level, trueInputVoltage, 1);
         break;
     case 2:
-        drawGraph(-1);
-        drawPoint((int)level, (int)trueInputVoltage, -1);
+        drawGraph(level, trueInputVoltage, -1);
+        drawPoint(level, trueInputVoltage, -1);
         break;
     }
 
     u8g2.sendBuffer();
 }
 
-void drawGraph(int8_t type) {
+void drawGraph(float x, float y, int8_t type) {
     u8g2.setFont(u8g2_font_ncenB08_tr);
 
     if (type == 1) {
         u8g2.setCursor(0, 64);
-        u8g2.print("Niveau");
-        u8g2.setCursor(84, 8);
-        u8g2.print("Tension");
+        u8g2.print("Niveau: " + String((int)x) + "mm");
+        u8g2.setCursor(48, 8);
+        u8g2.print("Tension: " + String(y) + "V");
         u8g2.drawLine(0, 0, 128, 64);
     } else {
-        u8g2.setCursor(90, 64);
-        u8g2.print("Niveau");
+        x = map(x, 0, 393, 393, 0);
+        u8g2.setCursor(42, 64);
+        u8g2.print("Niveau: " + String((int)x) + "mm");
         u8g2.setCursor(0, 8);
-        u8g2.print("Tension");
+        u8g2.print("Tension: " + String(y) + "V");
         u8g2.drawLine(0, 64, 128, 0);
     }
 }
 
-void drawPoint(int x, int y, int8_t type) {
+void drawPoint(float x, float y, int8_t type) {
     u8g2.setFont(u8g2_font_ncenB10_tr);
 
     if (type == 1) {
         x = map(x, 0, 393, 0, 128);
         y = map(y, 0, b, 64, 0);
-        u8g2.drawRFrame(x - 2, y - 2, 4, 4, 2);
+        u8g2.drawRFrame(x - 4, y - 4, 4, 4, 2);
     } else {
-        x = map(x, 0, 393, 0, 128);
-        y = map(y, 0, b, 0, 64);
-        u8g2.drawRFrame(x - 2, y - 2, 4, 4, 2);
+        x = map(x, 0, 393, 128, 0);
+        y = map(y, 0, b, 64, 0);
+        u8g2.drawRFrame(x - 4, y - 4, 4, 4, 2);
     }
 }
--- Programs/Tests/Graph_Display/Graph_Display.ino
+++ Programs/Tests/Graph_Display/Graph_Display.ino
@@ -3,7 +3,7 @@
 
 const float a = -0.022;
 const float b = 8.646;
-int8_t type = -1;
+int8_t type = 1;
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
@@ -11,7 +11,7 @@ int voltageInPin = 525;
 float inputVoltage, trueInputVoltage;
 float level, voltage;
 
-void drawGraph(int8_t);
+void drawGraph(int, int, int8_t);
 void drawPint(int, int, int8_t);
 
 void setup() {
@@ -32,29 +32,30 @@ void loop() {
 
     u8g2.clearBuffer();
 
-    drawGraph(type);
-
-    int x = 300;
+    int x = 200;
     int y = a * x + b;
+
+    drawGraph(x, y, type);
     drawPoint(x, y, type);
 
     u8g2.sendBuffer();
 }
 
-void drawGraph(int8_t type) {
+void drawGraph(int x, int y, int8_t type) {
     u8g2.setFont(u8g2_font_ncenB08_tr);
 
     if (type == 1) {
         u8g2.setCursor(0, 64);
-        u8g2.print("Niveau");
-        u8g2.setCursor(84, 8);
-        u8g2.print("Tension");
+        u8g2.print("Niveau: " + String(x) + "mm");
+        u8g2.setCursor(58, 8);
+        u8g2.print("Tension: " + String(y) + "V");
         u8g2.drawLine(0, 0, 128, 64);
     } else {
-        u8g2.setCursor(90, 64);
-        u8g2.print("Niveau");
+        y = map(y, 0, b, b, 0);
+        u8g2.setCursor(42, 64);
+        u8g2.print("Niveau: " + String(x) + "mm");
         u8g2.setCursor(0, 8);
-        u8g2.print("Tension");
+        u8g2.print("Tension: " + String(y) + "V");
         u8g2.drawLine(0, 64, 128, 0);
     }
 }
